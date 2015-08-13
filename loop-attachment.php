ee805f3ce6ad4a87b07fb8701963d7ab9742b22b
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-08-13 10:45:32 +0200

    attachments parent_id firts attachment see posts table

--- loop-attachment.php
+++ loop-attachment.php
@@ -6,17 +6,19 @@
             $args = [
                 'post_type'   => 'attachment',
                 'numberposts' => -1, // tous <=> -1
-                'post_parent' => $post->ID, // associé à notre article
+                'post_parent' => $post->ID, // associé à notre article pour les images uploadé la première fois, voir parent_id dans la table posts
             ];
 
             $attachments = get_posts($args); // API SQL WP
+
             if ($attachments) {
                 echo '<ul class="attachment">';
                 foreach ($attachments as $attachment) {
                     echo '<li>';
                     echo wp_get_attachment_image($attachment->ID, 'thumbnail');
                     echo '</li>';
-                    echo '<span class="attachment__title">titre</span>';
+                    echo '<span class="attachment__title">' . $post->title . '
+                    </span>';
                 }
                 echo '</ul>';
             }
