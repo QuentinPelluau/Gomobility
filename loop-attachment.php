9dabf2d82cecdca3b2204807d39e4ed9f29a7e5b
Antoine07 <antoine.lucsko@wanadoo.fr>
2015-08-13 10:23:12 +0200

    attachments

---
+++ loop-attachment.php
@@ -0,0 +1,28 @@
+<?php if (have_posts()): ?>
+    <?php while (have_posts()): the_post(); ?>
+        <article>
+            <h1><?php the_title(); ?></h1>
+            <?php
+            $args = [
+                'post_type'   => 'attachment',
+                'numberposts' => -1, // tous <=> -1
+                'post_parent' => $post->ID, // associé à notre article
+            ];
+
+            $attachments = get_posts($args); // API SQL WP
+            if ($attachments) {
+                echo '<ul class="attachment">';
+                foreach ($attachments as $attachment) {
+                    echo '<li>';
+                    echo wp_get_attachment_image($attachment->ID, 'thumbnail');
+                    echo '</li>';
+                    echo '<span class="attachment__title">titre</span>';
+                }
+                echo '</ul>';
+            }
+            ?>
+        </article>
+    <?php endwhile; ?>
+<?php else: ?>
+    <p>désolé aucun contenu pour l'instant</p>
+<?php endif; ?>
